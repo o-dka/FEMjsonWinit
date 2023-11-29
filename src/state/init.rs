f364eeaa396211b14e0a6014e4ce3c0b59c74c4f
odka <63544489+o-dka@users.noreply.github.com>
2023-11-29 12:10:29 +0300

    updated  init.rs

--- src/state/init.rs
+++ src/state/init.rs
@@ -1,14 +1,5 @@
-#![allow(dead_code)]
 use winit::window::Window;
-use cgmath::*;
 
-#[rustfmt::skip]
-pub const OPENGL_TO_WGPU_MATRIX: Matrix4<f32> = Matrix4::new(
-    1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 0.0,
-    0.0, 0.0, 0.5, 0.0,
-    0.0, 0.0, 0.5, 1.0,
-);
 pub struct InitWgpu {
     pub instance: wgpu::Instance,
     pub surface: wgpu::Surface,
